00bca845a52914bc76f7300b46767d710fa57158
Izzatbek Ulkanov <izzatbekulkanov@gmail.com>
2023-10-11 13:38:06 +0500

    .

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Contact, Category, News
 
 # Register your models here.
+
+@admin.register(Contact)
+class Contact(admin.ModelAdmin):
+    def get_ordering(self, request):
+        return ['id']
+
+    list_display = ['id', 'name', 'email']
+    search_fields = ['name', 'email']
+    list_display_links = ['email', 'name']
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    def get_ordering(self, request):
+        return ['id']
+
+    list_display = ['id', 'title', 'body']
+    search_fields = ['title', 'body']
+    list_display_links = ['title', 'body']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    def get_ordering(self, request):
+        return ['id']
+
+    list_display = ['name']
+    search_fields = ['name']
+    list_display_links = ['name']
--- news/views.py
+++ news/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from .models import Contact
+from .forms import ContactForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -7,7 +11,22 @@ def main(request):
     return render(request, 'base/main.html')
 def error404(request):
     return render(request, 'pages/404.html')
+
+
+
 def contact(request):
-    return render(request, 'pages/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Sahifani qaytarish uchun 'return' ishlatish
+            return render(request, 'base/main.html')  # 'main' o'rniga o'zingizning shabloningizni qo'yishingiz kerak
+    else:
+        form = ContactForm()
+    context = {
+        "form": form
+    }
+    return render(request, 'pages/contact.html', context)
+
 def single_page(request):
     return render(request, 'pages/single_page.html')
